d168dc1e819994eee82eeb4bc06e53d12ff25d85
anam-alam <anamit3010@gmail.com>
2025-05-05 23:27:04 +0530

    added functionalities to the player

--- main.py
+++ main.py
@@ -5,20 +5,42 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
 
+    
+    
+    updatable  = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type ==pygame.QUIT:
                 return
+        
+
+        updatable.update(dt)
+        
         screen.fill(color="black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+
+        
         pygame.display.flip()
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
+        
 
 
     
